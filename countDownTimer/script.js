1d8197094c58fce8aafe2ccf77ee84bd4aa9c47a
Nsanzimfura Ericky <erickykress1@gmail.com>
2022-05-02 08:24:16 +0200

    Add files via upload

---
+++ countDownTimer/script.js
@@ -0,0 +1,39 @@
+
+const dayEl = document.getElementById('day');
+const hourEl= document.getElementById('hour');
+const minutEl= document.getElementById('minute');
+const secondEl= document.getElementById('second');
+
+
+const wishedDay = '01 May 2022';
+
+function countDownTimer(){
+
+    const wishedDate =  new Date(wishedDay);
+    const currentDate = new Date();
+    const totalSeconds = (wishedDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds )% 60;
+
+       dayEl.innerHTML = days;
+        hourEl.innerHTML = hours;
+
+        if (seconds <10)   
+                {secondEl.innerHTML = "0" + seconds;} 
+        else if(minutes < 10)
+                {  minutEl.innerHTML = "0" + minutes;}
+        else{
+            
+            minutEl.innerHTML = minutes;
+            secondEl.innerHTML = seconds;
+        }
+   
+}
+
+
+setInterval(countDownTimer, 1000);
+
+
